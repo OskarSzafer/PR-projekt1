8f5051f016543f5885c4f051d843c062d8e59e8e
Aleksandra Ostrowska <116283894+sandaczsands@users.noreply.github.com>
2025-04-19 14:01:35 +0200

    added k4.c and k5.c

---
+++ k4.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
+#include <time.h>
+#include <omp.h>  
+
+// Compile: gcc k4.c -o k4.o -lm -fopenmp -O3
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        printf("Użycie: %s <dolny_zakres> <gorny_zakres>\n", argv[0]);
+        return 1;
+    }
+
+    int m = atoi(argv[1]);
+    int n = atoi(argv[2]);
+
+    if (m > n || m < 2) {
+        printf("Zakres nieprawidłowy. Upewnij się, że m >= 2 i m <= n.\n");
+        return 1;
+    }
+
+    clock_t start = clock();
+
+    int range = n - m + 1;
+    bool* result = (bool*)malloc(range * sizeof(bool));
+    memset(result, true, range * sizeof(bool));
+
+    int sqrt_n = (int)sqrt(n) + 1;
+    bool* primeArray = (bool*)malloc((n + 1) * sizeof(bool));
+    memset(primeArray, true, (n + 1) * sizeof(bool));
+
+    for (int i = 2; i * i * i * i <= n; i++) {
+        if (primeArray[i]) {
+            for (int j = i * i; j * j <= n; j += i) {
+                primeArray[j] = false;
+            }
+        }
+    }
+
+    #pragma omp parallel for 
+    for (int i = 2; i * i <= n; i++) {
+        if (primeArray[i]) {
+            int firstMultiple = (m / i);
+            if (firstMultiple <= 1) {
+                firstMultiple = i + i;
+            } else if (m % i) {
+                firstMultiple = (firstMultiple * i) + i;
+            } else {
+                firstMultiple = (firstMultiple * i);
+            }
+
+            for (int j = firstMultiple; j <= n; j += i) {
+                result[j - m] = false;
+            }
+        }
+    }
+
+    printf("Liczby pierwsze w zakresie [%d, %d]:\n", m, n);
+    for (int i = 0; i < range; i++) {
+        if (result[i]) {
+            printf("%d ", i + m);
+        }
+    }
+    printf("\n");
+
+    clock_t end = clock();
+    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Czas przetwarzania: %.6f sekund\n", time_spent);
+
+    free(result);
+    free(primeArray);
+
+    return 0;
+}
---
+++ k5.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
+#include <time.h>
+
+// gcc k1.c -o k1.o -lm -fopenmp -O3
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        printf("Użycie: %s <dolny_zakres> <gorny_zakres>\n", argv[0]);
+        return 1;
+    }
+
+    int m = atoi(argv[1]);
+    int n = atoi(argv[2]);
+
+    if (m > n || m < 2) {
+        printf("Zakres nieprawidłowy. Upewnij się, że m >= 2 i m <= n.\n");
+        return 1;
+    }
+
+    clock_t start = clock();
+
+    int range = n - m + 1;
+    bool* result = (bool*)malloc(range * sizeof(bool));
+    memset(result, true, range * sizeof(bool));
+
+    int sqrt_n = (int)sqrt(n) + 1;
+    bool* primeArray = (bool*)malloc(sqrt_n * sizeof(bool));
+    memset(primeArray, true, sqrt_n * sizeof(bool));
+
+    for (int i = 2; i * i <= sqrt_n; i++) {
+        if (primeArray[i]) {
+            for (int j = i * i; j < sqrt_n; j += i) {
+                primeArray[j] = false;
+            }
+        }
+    }
+
+    int blockSize = 1000;
+    int numberOfBlocks = (n - m) / blockSize;
+
+    if ((n - m) % blockSize != 0) {
+        numberOfBlocks++;
+    }
+
+    #pragma omp parallel for
+    for (int i = 0; i < numberOfBlocks; i++) {
+        int low = m + i * blockSize;
+        int high = low + blockSize;
+        if (high > n) {
+            high = n;
+        }
+
+        for (int j = 2; j * j <= high; j++) {
+            if (primeArray[j]) {
+                int firstMultiple = (low / j);
+
+                if (firstMultiple <= 1) {
+                    firstMultiple = j + j;
+                } else {
+                    if (low % j) {
+                        firstMultiple = (firstMultiple * j) + j;
+                    } else  {
+                        firstMultiple = (firstMultiple * j);
+                    }
+                }
+                
+                for (int k = firstMultiple; k <= high; k += j) {
+                    result[k - m] = false;
+                }
+            }
+        }
+    }
+
+    printf("Liczby pierwsze w zakresie [%d, %d]:\n", m, n);
+    for (int i = 0; i < range; i++) {
+        if (result[i]) {
+            printf("%d ", i + m);
+        }
+    }
+    printf("\n");
+
+    clock_t end = clock();
+    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Czas przetwarzania: %.6f sekund\n", time_spent);
+
+    free(result);
+    free(primeArray);
+
+    return 0;
+}
