e615b3d384539c6389236aab94605650073e3cb6
Aleksandra Ostrowska <116283894+sandaczsands@users.noreply.github.com>
2025-04-19 14:11:02 +0200

    Update k3.c

--- k3.c
+++ k3.c
@@ -30,7 +30,6 @@ int main(int argc, char* argv[]) {
     bool* primeArray = (bool*)malloc((n + 1) * sizeof(bool));
     memset(primeArray, true, (n + 1) * sizeof(bool));
 
-    // Sito Eratostenesa bez lokalności dostępu do danych
     for (int i = 2; i * i * i * i <= n; i++) {
         if (primeArray[i] == true) {
             for (int j = i * i; j * j <= n; j += i) {
